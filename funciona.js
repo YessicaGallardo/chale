edf8b7d11e185b53dcfec48ec5ec6583b8ef3a22
YessicaGallardo <princss.yes@gmail.com>
2020-08-20 18:57:19 -0500

    Creacion de archivos frontend

---
+++ funciona.js
@@ -0,0 +1,27 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+$('#logueo').click(function(){
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then(function(result){
+        console.log(result.user);
+        almacenaDatos(result.user);
+        $('#logueo').hide();
+        $('principal').append("<img src='"+result.user.photoURL+"'/>")
+    });
+});
+
+function almacenaDatos(user){
+    var datosDelUsuario={
+        nombre:user.displayName,
+        email:user.email,
+        foto:user.photoURL,
+        uid:user.uid
+}
+    firebase.database().ref("datosInicioDeSesion/"+ user.uid)
+    .push(datosDelUsuario)
+}
+firebase.database().ref("datosInicioDeSesion")
+.on("child_added",function(s){
+    var usuario2=s.val();
+    $('#principal').append("<img width='150px'scr=' "+user.foto+"'/>");
+})
